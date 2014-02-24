b14c0b8ed9ca4ff3917d6a6c51199435abb26bf8
Louis Vanduyfhuys <Louis.Vanduyfhuys@UGent.be>
2014-02-24 09:31:49 +0100

    Update setup.py header

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# QuickFF is a python library to quickly derive force fields from ab initio training data.
-# Copyright (C) 2013 Louis Vanduyfhuys <Louis.Vanduyfhuys@UGent.be>,
-# Center for Molecular Modeling (CMM), Ghent University, Ghent, Belgium;
-# all rights reserved unless otherwise stated.
+#QuickFF is a code to quickly derive accurate force fields from ab initio input.
+#Copyright (C) 2012 - 2014 Louis Vanduyfhuys <Louis.Vanduyfhuys@UGent.be>
+#Steven Vandenbrande <Steven.Vandenbrande@UGent.be>, 
+#Toon Verstraelen <Toon.Verstraelen@UGent.be>, Center for Molecular Modeling
+#(CMM), Ghent University, Ghent, Belgium; all rights reserved unless otherwise
+#stated.
 #
-# This file is part of QuickFF.
+#This file is part of QuickFF.
 #
-# QuickFFis free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 3
-# of the License, or (at your option) any later version.
+#QuickFF is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 3
+#of the License, or (at your option) any later version.
 #
-# QuickFF is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#QuickFF is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, see <http://www.gnu.org/licenses/>
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 #--
 
