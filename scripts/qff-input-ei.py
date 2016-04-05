c031ffb18b6e10f295e4adb638ff457596733829
Louis Vanduyfhuys <Louis.Vanduyfhuys@UGent.be>
2016-04-05 10:11:10 +0200

    Added missing scripts, and added output filename option to qff-input-ei.py

---
+++ scripts/qff-input-ei.py
@@ -0,0 +1,125 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# QuickFF is a code to quickly derive accurate force fields from ab initio input.
+# Copyright (C) 2012 - 2016 Louis Vanduyfhuys <Louis.Vanduyfhuys@UGent.be>
+# Steven Vandenbrande <Steven.Vandenbrande@UGent.be>,
+# Toon Verstraelen <Toon.Verstraelen@UGent.be>, Center for Molecular Modeling
+# (CMM), Ghent University, Ghent, Belgium; all rights reserved unless otherwise
+# stated.
+#
+# This file is part of QuickFF.
+#
+# QuickFF is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# QuickFF is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+#--
+
+from quickff.tools import guess_ffatypes, get_ei_radii, average, charges_to_bcis
+from quickff.io import read_abinitio, make_yaff_ei, read_bci_constraints
+from yaff import System
+from optparse import OptionParser
+
+import h5py
+
+def parse():
+    usage = '%prog [options] fn_sys fn_hdf5 path'
+    description  = 'This script will read the atomic charges from the input '
+    description += 'arguments and make the Yaff parameters file for use with '
+    description += 'the QuickFF option --ei. fn_sys is any file from which the '
+    description += 'system can be extracted (MolMod CHK, Gaussian FCHK, XYZ, '
+    description += '...). Path represents the path to the charges, which are '
+    description += 'assumed to be stored in the file fn_hdf5 in the dataset '
+    description += '/path/charges.'
+    parser = OptionParser(usage=usage, description=description)
+    parser.add_option(
+        '--ffatypes', default=None,
+        help='Assign atom types in the system, through either on of three '
+             'possible routines. If FFATYPES is a string equal to either '
+             'low,medium, high or highest, atom types will be assigned through '
+             'the automatic built-in detection (see documentation). If FFATYPES '
+             'is a different string, it is assumed to be the name of a file '
+             'containing a list of atype names and Yaff ATSELECT rules to '
+             'defined the atom types. If FFATYPES is None, the atom types are '
+             'assumed to be defined in the input files. [default=%default]'
+    )
+    parser.add_option(
+        '--gaussian', default=False, action='store_true',
+        help='Use gaussian smeared charges. The radii are taken from the '
+             'dataset /path/radii if it exists, otherwise the radii are '
+             'estimated according to the procedure of Chen et al. See '
+             '``quickff.tools.get_ei_radii`` for more info.'
+    )
+    parser.add_option(
+        '--bci', default=False, action='store_true',
+        help='Convert averaged atomic charges to bond charge increments, i.e. '
+             'charge transfers along the chemical bonds in the system. In this '
+             'way, one is certain of ending up with a globally neutral system '
+             'even if bcis from different systems are combined. This option '
+             'requires the definition of bonds, if the bonds cannot be read '
+             'from the system file, they will be estimated from the interatomic '
+             'distances using the detect_bonds routine in the Yaff System '
+             'class. [default=%default]'
+    )
+    parser.add_option(
+        '--bci-constraints', default=None,
+        help='A file containing constraints for the charge to bci fit in a '
+             'master: [slave0, slave1, ...] format. A new line should be used '
+             'for each master and the format is insensitive towards spaces.'
+    )
+    parser.add_option(
+        '-o', '--output', default=None,
+        help='Name of the Yaff file to write the parameters to.'
+             ' [default=pars_ei_${PATH}.txt]'
+    )
+    options, args = parser.parse_args()
+    assert len(args)==3, 'Exactly three arguments are required: a system file, a HDF5 file and the path to the charges.'
+    return options, args
+
+def main():
+    options, args = parse()
+    fn_sys, fn_hdf5, path = args
+    if fn_sys.endswith('.fchk'):
+        numbers, coords, energy, grad, hess, masses, rvecs, pbc = read_abinitio(fn_sys)
+        system = System(numbers, coords, rvecs=None, charges=None, radii=None, masses=masses)
+        system.detect_bonds()
+    else:
+        system = System.from_file(fn_sys)
+    if options.ffatypes is not None:
+        guess_ffatypes(system, options.ffatypes)
+    ffatypes = [system.ffatypes[i] for i in system.ffatype_ids]
+    h5 = h5py.File(fn_hdf5)
+    if not path in h5 or 'charges' not in h5[path]:
+        raise IOError('Given file %s does not contain dataset %s/charges' %(fn_hdf5, path))
+    radii = None
+    if options.gaussian:
+        if 'radii' in h5[path]:
+            radii = average(h5['%s/radii' %path][:], ffatypes, fmt='dict')
+        else:
+            radii = average(get_ei_radii(system.numbers), ffatypes, fmt='dict')
+    if options.output is None:
+        fn_out = 'pars_ei_%s.txt' %path.replace('/', '_')
+    else:
+        fn_out = options.output
+    if options.bci:
+        constraints = {}
+        if options.bci_constraints is not None:
+            constraints = read_bci_constraints(options.bci_constraints)
+        charges = average(h5['%s/charges' %path][:], ffatypes, fmt='full')
+        bcis = charges_to_bcis(charges, ffatypes, system.bonds, constraints=constraints)
+        make_yaff_ei(fn_out, None, bcis=bcis, radii=radii)
+    else:
+        charges = average(h5['%s/charges' %path][:], ffatypes, fmt='dict')
+        make_yaff_ei(fn_out, charges, radii=radii)
+
+if __name__=='__main__':
+    main()
---
+++ scripts/qff.py
@@ -0,0 +1,252 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# QuickFF is a code to quickly derive accurate force fields from ab initio input.
+# Copyright (C) 2012 - 2016 Louis Vanduyfhuys <Louis.Vanduyfhuys@UGent.be>
+# Steven Vandenbrande <Steven.Vandenbrande@UGent.be>,
+# Toon Verstraelen <Toon.Verstraelen@UGent.be>, Center for Molecular Modeling
+# (CMM), Ghent University, Ghent, Belgium; all rights reserved unless otherwise
+# stated.
+#
+# This file is part of QuickFF.
+#
+# QuickFF is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# QuickFF is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>
+#
+#--
+
+from quickff.program import __all__ as allowed_programs, __dict__ as program_modes
+from quickff.log import log
+from quickff.tools import guess_ffatypes
+from quickff.reference import SecondOrderTaylor, YaffForceField
+from quickff.io import read_abinitio
+from quickff.paracontext import paracontext
+
+from yaff import System, ForceField, Cell
+
+from molmod.units import angstrom
+from molmod.io.chk import load_chk
+
+from optparse import OptionParser, OptionGroup
+import sys, os
+
+def parse():
+    usage = '%prog [options] fn1 [fn2 [...]]'
+    description  = 'This script will apply QuickFF to derive a covalent force '
+    description += 'field for the given system. The arguments fn1, fn2, ... '
+    description += 'represent all input files that specify the system and the '
+    description += 'ab initio reference data. Files later in the list '
+    description += 'overwrite earlier files'
+    parser = OptionParser(usage=usage, description=description)
+    parser.add_option(
+        '--scoop', default=False, action='store_true',
+        help='Flag to enable parallelisation using SCOOP. With SCOOP, the '
+             'command to run QuickFF is slightly different, the absolute path '
+             'to quickff.py should be used. For example, to run on 4 cores:\n'
+             'python -m scoop -n4 /path/to/%prog --scoop [options] fns\n'
+             '[default=%default]'
+    )
+    parser.add_option(
+        '-s', '--silent', default=False, action='store_true',
+        help='Swith of all logging completely, overwrites all other verbosity '
+             'options. [default=%default]'
+    )
+    parser.add_option(
+        '-v', '--verbose', default=False, action='store_true',
+        help='Increases verbosity, is overwriten if SILENT is switched on. '
+             '[default=%default]'
+    )
+    parser.add_option(
+        '-l', '--logfile', default=None,
+        help='Redirect logger output to a file with the given name. '
+             '[default=%default]'
+    )
+    #General settings options
+    settings = OptionGroup(parser, 'General', 'General specifications of the program')
+    settings.add_option(
+        '-m', '--program-mode', default='DeriveNonDiagFF',
+        help='Specify the program mode which defines the set of instructions '
+             'that will be executed. Allowed strings are the names of the '
+             'program classes defined in quickff/program.py, which are: '
+             ''+', '.join([prog for prog in allowed_programs if not prog=='BaseProgram'])+' (case sensitive). [default=%default]'
+    )
+    settings.add_option(
+        '--fn-traj', default=None,
+        help='Name for the file to read/write the perturbation trajectories '
+             'from/to. If the given file exists, the trajectories are read '
+             'from the file. Otherwise, the trajectories are written to the '
+             'given file. [default=%default]'
+    )
+    settings.add_option(
+        '--only-traj', default=None,
+        help='Construct the perturbation trajectory only for the terms with '+\
+             'the given basenames. This options is only applied in the ' +\
+             'MakeTrajectories program.'
+    )
+    settings.add_option(
+        '-e', '--ener-traj', default=False, action='store_true',
+        help='Plot the various energy contributions along the perturbation '
+             'trajectories to. [default=%default]'
+    )
+    settings.add_option(
+        '-x', '--xyz-traj', default=False, action='store_true',
+        help='Write the perturbation trajectories in XYZ format. '
+             '[default=%default]'
+    )
+    settings.add_option(
+        '--suffix', default='',
+        help = "Suffix that will be added to all output files. [default='']"
+    )
+    parser.add_option_group(settings)
+    #Force field options
+    ff = OptionGroup(parser, 'Force Field', 'All options related to the definition and derivation of the force field.')
+    ff.add_option(
+        '--ei', default=None,
+        help='A Yaff parameters file defining the electrostatic contribution '
+             'of the force field. [default=%default]'
+    )
+    ff.add_option(
+        '--vdw', default=None,
+        help='A Yaff parameters file defining the van der Waals contribution '
+             'of the force field. [default=%default]'
+    )
+    ff.add_option(
+        '--covres', default=None,
+        help='A Yaff parameters file defining a residual contribution to the '
+             'covalent part of the force field. [default=%default]'
+    )
+    parser.add_option_group(ff)
+    #System options
+    system = OptionGroup(parser, 'System', 'All options related to the definition of the system.')
+    system.add_option(
+        '--ffatypes', default=None,
+        help='Assign atom types in the system, through either on of three '
+             'possible routines. If FFATYPES is a string equal to either '
+             'low,medium, high or highest, atom types will be assigned through '
+             'the automatic built-in detection (see documentation). If FFATYPES '
+             'is a different string, it is assumed to be the name of a file '
+             'containing a list of atype names and Yaff ATSELECT rules to '
+             'defined the atom types. If FFATYPES is None, the atom types are '
+             'assumed to be defined in the input files. [default=%default]'
+    )
+    parser.add_option_group(system)
+    options, args = parser.parse_args()
+    assert len(args)>0, 'No input files found.'
+    return options, args
+
+def main():
+    options, fns = parse()
+    #define logger
+    if options.silent:
+        log.set_level('silent')
+    else:
+        if options.verbose:
+            log.set_level('highest')
+        if options.logfile is not None and isinstance(options.logfile, str):
+            log.write_to_file(options.logfile)
+    with log.section('QFF', 1, timer='Initializing'):
+        log.dump('Initializing system')
+        #read system and ab initio reference
+        system = None
+        energy = 0.0
+        grad = None
+        hess = None
+        rvecs = None
+        for fn in fns:
+            if fn.endswith('.fchk'):
+                numbers, coords, energy, grad, hess, masses, rvecs, pbc = read_abinitio(fn)
+                if system is None:
+                    system = System(
+                        numbers, coords, rvecs=None, charges=None, radii=None,
+                        masses=masses
+                    )
+                else:
+                    system.pos = coords.copy()
+                    system.cell = Cell(rvecs)
+                    system.numbers = numbers.copy()
+                    if masses is not None: system.masses = masses.copy()
+                    system._init_derived()
+            elif fn.endswith('.chk'):
+                sample = load_chk(fn)
+                if 'energy' in sample.keys():       energy = sample['energy']
+                if 'grad' in sample.keys():         grad = sample['grad']
+                elif 'gradient' in sample.keys():   grad = sample['gradient']
+                if 'hess' in sample.keys():         hess = sample['hess']
+                elif 'hessian' in sample.keys():    hess = sample['hessian']
+                if system is None:
+                    system = System.from_file(fn)
+                else:
+                    if 'pos' in sample.keys():          system.pos = sample['pos']
+                    elif 'coords' in sample.keys():     system.pos = sample['coords']
+                    if 'rvecs' in sample.keys():        system.cell = Cell(sample['rvecs'])
+                    elif 'cell' in sample.keys():       system.cell = Cell(sample['cell'])
+                    if 'bonds' in sample.keys():        system.bonds = sample['bonds']
+                    if 'ffatypes' in sample.keys():     system.ffatypes = sample['ffatypes']
+                    if 'ffatype_ids' in sample.keys():  system.ffatype_ids = sample['ffatype_ids']
+                    system._init_derived()
+            else:
+                raise NotImplementedError('File format for %s not supported' %fn)
+        assert system is not None, 'No system could be defined from input'
+        assert grad is not None, 'No ab initio gradient found in input'
+        assert hess is not None, 'No ab initio hessian found in input'
+        #complete the system information
+        if system.bonds is None: system.detect_bonds()
+        if system.masses is None: system.set_standard_masses()
+        if system.ffatypes is None:
+            if options.ffatypes in ['low', 'medium', 'high', 'highest']:
+                guess_ffatypes(system, options.ffatypes)
+            elif options.ffatypes is not None:
+                raise NotImplementedError('Guessing atom types from %s not implemented' %options.ffatypes)
+            else:
+                raise AssertionError('No atom types defined')
+        #construct ab initio reference
+        ai = SecondOrderTaylor('ai', coords=coords, energy=energy, grad=grad, hess=hess)
+        #detect a priori defined contributions to the force field
+        refs = []
+        if options.ei is not None:
+            rcut = 50*angstrom
+            if rvecs is None:
+                ff = ForceField.generate(system, options.ei, rcut=50*angstrom)
+            else:
+                ff = ForceField.generate(system, options.ei, rcut=20*angstrom, alpha_scale=3.2, gcut_scale=1.5, smooth_ei=True)
+            refs.append(YaffForceField('EI', ff))
+        if options.vdw is not None:
+            ff = ForceField.generate(system, options.vdw, rcut=20*angstrom)
+            refs.append(YaffForceField('vdW', ff))
+        if options.covres is not None:
+            ff = ForceField.generate(system, options.covres)
+            refs.append(YaffForceField('Cov res', ff))
+    #define quickff program
+    assert options.program_mode in allowed_programs, \
+        'Given program mode %s not allowed. Choose one of %s' %(
+            options.program_mode,
+            ', '.join([prog for prog in allowed_programs if not prog=='BaseProgram'])
+        )
+    mode = program_modes[options.program_mode]
+    only_traj = 'PT_ALL'
+    if options.only_traj is not None: only_traj = options.only_traj.split(',')
+    program = mode(
+        system, ai, ffrefs=refs,
+        fn_traj=options.fn_traj, only_traj=only_traj,
+        plot_traj=options.ener_traj, xyz_traj=options.xyz_traj,
+        suffix=options.suffix
+    )
+    #run program
+    program.run()
+
+#Set the parallel context if scoop is enabled. This should be performed outside
+#the __main__ block to ensure the set the context for all workers.
+if '--scoop' in sys.argv[1:]:
+    paracontext.use_scoop()
+
+if __name__=='__main__':
+    main()
