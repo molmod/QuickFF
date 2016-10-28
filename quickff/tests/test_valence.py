c67e321737647edb60610a8433d0f36b4b6c35ca
Louis Vanduyfhuys <Louis.Vanduyfhuys@UGent.be>
2016-10-28 11:43:35 +0200

    Disabled dihedrals tests for diheds close to 0 or 180 deg due to yaff instability and added ethane to share/systems

--- quickff/tests/test_valence.py
+++ quickff/tests/test_valence.py
@@ -1,4 +1,5 @@
 from molmod.units import angstrom, kjmol, rad, deg
+from molmod.ic import _dihed_angle_low, dihed_angle
 
 from quickff.valence import ValenceFF
 from quickff.tools import guess_ffatypes
@@ -70,7 +71,6 @@ def get_analytic_numeric_hessian(valence, term, **ffpars):
         assert len(term2.slaves)==0
         if term2.index!=term.index:
             valence.set_params(term2.index, rv0=0.0, fc=0.0)
-    #print [(i,valence.system.ffatypes[valence.system.ffatype_ids[i]]) for i in term.get_atoms()]
     #compute hcov using built-in function (which uses yaff routine)
     ref = valence.get_hessian_contrib(term.index)
     #compute hcov numerically using valence.calc_energy
@@ -87,11 +87,40 @@ def get_analytic_numeric_hessian(valence, term, **ffpars):
             Dj = Dj.reshape([natoms, 3])
             tmp  = valence.calc_energy(valence.system.pos + Di + Dj)
             tmp -= valence.calc_energy(valence.system.pos + Di - Dj)
-            tmp -= valence.calc_energy(valence.system.pos - Di + Dj)
+            tmp -= valence.calc_energy(valence.system.pos -     Di + Dj)
             tmp += valence.calc_energy(valence.system.pos - Di - Dj)
             num[i,j] = tmp/(2.0*eps)**2
     num = 0.5*(num+num.T)
     return ref, num
+
+def get_indices_zero_nonzero(term, natoms):
+    inonzero = [[],[]]
+    izero = [[],[]]
+    for i in xrange(natoms):
+        iindices = [3*i,3*i,3*i, 3*i+1,3*i+1,3*i+1, 3*i+2,3*i+2,3*i+2]
+        for j in xrange(natoms):
+            jindices = [3*j,3*j+1,3*j+2, 3*j,3*j+1,3*j+2, 3*j,3*j+1,3*j+2] 
+            if i in term.get_atoms() and j in term.get_atoms():
+                inonzero[0] += iindices
+                inonzero[1] += jindices
+            else:
+                izero[0] += iindices
+                izero[1] += jindices
+    return inonzero, izero
+
+def get_dihedral_angle(term, system):
+    dihed = term.get_atoms()
+    if system.cell.nvec>0:
+        d10 = system.pos[dihed[0]] - system.pos[dihed[1]]
+        d12 = system.pos[dihed[2]] - system.pos[dihed[1]]
+        d23 = system.pos[dihed[3]] - system.pos[dihed[2]]
+        system.cell.mic(d10)
+        system.cell.mic(d12)
+        system.cell.mic(d23)
+        return _dihed_angle_low(d10, d12, d23, 0)[0]
+    else:
+        rs = np.array([system.pos[j] for j in dihed])
+        return dihed_angle(rs)[0]
 
 def check_hessian_bonds(name, tol=1e-3*kjmol/angstrom**2):
     with log.section('NOSETST', 2):
@@ -99,13 +128,18 @@ def check_hessian_bonds(name, tol=1e-3*kjmol/angstrom**2):
         guess_ffatypes(system, 'highest')
         valence = ValenceFF(system)
     for term in valence.iter_terms('BONDHARM'):
+        inonzero, izero = get_indices_zero_nonzero(term, len(system.numbers))
         rv = np.random.uniform(low=1.00, high=2.00)*angstrom
         fc = np.random.uniform(low=1000, high=3000)*kjmol/angstrom**2
         ref, num = get_analytic_numeric_hessian(valence, term, fc=fc, rv0=rv)
-        nonzeros = np.where(ref!=0.0)
+        #assert that hessian elements of atoms not part of the current bond
+        #are zero
+        if len(izero[0])>0:
+            assert (abs(ref[izero])).max()<1e-12*kjmol/angstrom**2
+            assert (abs(num[izero])).max()<1e-12*kjmol/angstrom**2
         M = (abs(ref-num)).max()
         iM, jM = np.where(abs(ref-num)==M)[0][0], np.where(abs(ref-num)==M)[1][0]
-        print 'Bond    %2i (random FC=%8.3f kjmol/A^2    RV=%7.3f A  ):  MaxDev(%2i,%2i)=%.3e kjmol/A^2' %(term.index, fc/(kjmol/angstrom**2), rv/angstrom, iM, jM, M/(kjmol/angstrom**2))
+        print '%25s (random FC=%8.3f kjmol/A^2    RV=%7.3f A  ):  MaxDev(%2i,%2i)=%.3e kjmol/A^2' %(term.basename, fc/(kjmol/angstrom**2), rv/angstrom, iM, jM, M/(kjmol/angstrom**2))
         assert M<tol
     del system, valence, ref, num
 
@@ -115,13 +149,18 @@ def check_hessian_bends(name, tol=1e-3*kjmol/angstrom**2):
         guess_ffatypes(system, 'highest')
         valence = ValenceFF(system)
     for term in valence.iter_terms('BENDAHARM'):
+        inonzero, izero = get_indices_zero_nonzero(term, len(system.numbers))
         rv = np.random.uniform(low=10, high=170)*deg
         fc = np.random.uniform(low=100, high=1000)*kjmol/rad**2
         ref, num = get_analytic_numeric_hessian(valence, term, fc=fc, rv0=rv)
-        nonzeros = np.where(ref!=0.0)
+        #assert that hessian elements of atoms not part of the current bend
+        #are zero
+        if len(izero[0])>0:
+            assert (abs(ref[izero])).max()<1e-12*kjmol/angstrom**2
+            assert (abs(num[izero])).max()<1e-12*kjmol/angstrom**2
         M = (abs(ref-num)).max()
         iM, jM = np.where(abs(ref-num)==M)[0][0], np.where(abs(ref-num)==M)[1][0]
-        print 'Bend    %2i (random FC=%8.3f kjmol/rad^2  RV=%7.3f deg):  MaxDev(%2i,%2i)=%.3e kjmol/A^2' %(term.index, fc/(kjmol/rad**2), rv/deg, iM, jM, M/(kjmol/angstrom**2))
+        print '%25s (random FC=%8.3f kjmol/rad^2  RV=%7.3f deg):  MaxDev(%2i,%2i)=%.3e kjmol/A^2' %(term.basename, fc/(kjmol/rad**2), rv/deg, iM, jM, M/(kjmol/angstrom**2))
         assert M<tol
     del system, valence, ref, num
 
@@ -131,19 +170,23 @@ def check_hessian_dihedrals(name, tol=1e-3*kjmol/angstrom**2):
         guess_ffatypes(system, 'highest')
         valence = ValenceFF(system)
     for term in valence.iter_terms('TORSION'):
-        #if not term.index==21: continue
-        #vterm = valence.vlist.vtab[term.index]
-        #q0 = valence.iclist.ictab[vterm['ic0']]['value']
+        psi0 = get_dihedral_angle(term, system)
+        inonzero, izero = get_indices_zero_nonzero(term, len(system.numbers))
         rv = np.random.uniform(low=0, high=180)*deg #q0
         fc = np.random.uniform(low=10, high=50)*kjmol
         ref, num = get_analytic_numeric_hessian(valence, term, fc=fc, rv0=rv)
-        nonzeros = np.where(ref!=0.0)
-        #print ref[nonzeros]/(kjmol/angstrom**2)
-        #print num[nonzeros]/(kjmol/angstrom**2)
+        #assert that hessian elements of atoms not part of the current dihedral
+        #are zero
+        if len(izero[0])>0:
+            assert (abs(ref[izero])).max()<1e-12*kjmol/angstrom**2
+            assert (abs(num[izero])).max()<1e-12*kjmol/angstrom**2
         M = (abs(ref-num)).max()
         iM, jM = np.where(abs(ref-num)==M)[0][0], np.where(abs(ref-num)==M)[1][0]
-        print 'Torsion %2i (random FC=%8.3f kjmol        RV=%7.3f deg):  MaxDev(%2i,%2i)=%.3e kjmol/A^2' %(term.index, fc/kjmol, rv/deg, iM, jM, M/(kjmol/angstrom**2))
-        assert M<tol
+        print '%25s (eq=%.1f deg    random FC=%8.3f kjmol        RV=%7.3f deg):  MaxDev(%2i,%2i)=%.3e kjmol/A^2' %(term.basename, psi0/deg, fc/kjmol, rv/deg, iM, jM, M/(kjmol/angstrom**2))
+        if abs(abs(psi0)-180*deg)<1*deg or abs(psi0)<1*deg:
+            print ' ==> SKIPPED due to instable numerical implementation of derivatives in Yaff for values dihedrals to 0 or 180 deg'
+        else:
+            assert M<tol
     del system, valence, ref, num
 
 def check_hessian_oops(name, tol=1e-3*kjmol/angstrom**2):
@@ -152,22 +195,32 @@ def check_hessian_oops(name, tol=1e-3*kjmol/angstrom**2):
         guess_ffatypes(system, 'highest')
         valence = ValenceFF(system)
     for term in valence.iter_terms('/OOPDIST'):
+        inonzero, izero = get_indices_zero_nonzero(term, len(system.numbers))
         rv = 0.0
         fc = np.random.uniform(low=500, high=5000)*kjmol/angstrom**2
         ref, num = get_analytic_numeric_hessian(valence, term, fc=fc, rv0=rv)
-        nonzeros = np.where(ref!=0.0)
+        #assert that hessian elements of atoms not part of the current oop
+        #are zero
+        if len(izero[0])>0:
+            assert (abs(ref[izero])).max()<1e-12*kjmol/angstrom**2
+            assert (abs(num[izero])).max()<1e-12*kjmol/angstrom**2
         M = (abs(ref-num)).max()
         iM, jM = np.where(abs(ref-num)==M)[0][0], np.where(abs(ref-num)==M)[1][0]
-        print 'OopDist %2i (random FC=%8.3f kjmol/A^2    RV=%7.3f A  ):  MaxDev(%2i,%2i)=%.3e kjmol/A^2' %(term.index, fc/(kjmol/angstrom**2), rv/angstrom, iM, jM, M/(kjmol/angstrom**2))
+        print '%25s (random FC=%8.3f kjmol/A^2    RV=%7.3f A  ):  MaxDev(%2i,%2i)=%.3e kjmol/A^2' %(term.basename, fc/(kjmol/angstrom**2), rv/angstrom, iM, jM, M/(kjmol/angstrom**2))
         assert M<tol
     for term in valence.iter_terms('SQOOPDIST'):
+        inonzero, izero = get_indices_zero_nonzero(term, len(system.numbers))
         rv = np.random.uniform(low=0.01, high=0.1)*angstrom**2
         fc = np.random.uniform(low=500, high=5000)*kjmol/angstrom**4
         ref, num = get_analytic_numeric_hessian(valence, term, fc=fc, rv0=rv)
-        nonzeros = np.where(ref!=0.0)
+        #assert that hessian elements of atoms not part of the current oop
+        #are zero   
+        if len(izero[0])>0:
+            assert (abs(ref[izero])).max()<1e-12*kjmol/angstrom**2
+            assert (abs(num[izero])).max()<1e-12*kjmol/angstrom**2
         M = (abs(ref-num)).max()
         iM, jM = np.where(abs(ref-num)==M)[0][0], np.where(abs(ref-num)==M)[1][0]
-        print 'SQOpDst %2i (random FC=%8.3f kjmol/A^4    RV=%7.3f A^2):   MaxDev(%2i,%2i)=%.3e kjmol/A^2' %(term.index, fc/(kjmol/angstrom**4), rv/angstrom**2, iM, jM, M/(kjmol/angstrom**2))
+        print '%25s (random FC=%8.3f kjmol/A^4    RV=%7.3f A^2):   MaxDev(%2i,%2i)=%.3e kjmol/A^2' %(term.basename, fc/(kjmol/angstrom**4), rv/angstrom**2, iM, jM, M/(kjmol/angstrom**2))
         assert M<tol
     del system, valence, ref, num
 
