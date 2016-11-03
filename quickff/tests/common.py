fd030a86e5fb9abac51e28f2d72dd8e0da320c99
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2016-11-03 08:42:24 +0100

    Unit test for CHARMM22 output

--- quickff/tests/common.py
+++ quickff/tests/common.py
@@ -1,3 +1,11 @@
+from contextlib import contextmanager
+from glob import glob
+import os
+import shutil
+import tempfile
+
+import h5py as h5
+
 from quickff.io import read_abinitio
 
 from quickff.context import context
@@ -10,13 +18,37 @@ from yaff import System
 from quickff.log import log
 log.set_level('silent')
 
-__all__ = ['log', 'read_system']
+__all__ = ['log', 'read_system', 'tmpdir']
 
 def read_system(name):
-    fn = context.get_fn('systems/%s' %name)
+    # Load system data.
+    fn = context.get_fn(os.path.join('systems', name))
     numbers, coords, energy, grad, hess, masses, rvecs, pbc = read_abinitio(fn)
-    system = System(numbers, coords)
+    # Try to load charges.
+    charges = None
+    fns_wpart = glob(os.path.join(os.path.dirname(fn), '*wpart.h5'))
+    print os.path.join(os.path.dirname(name), '*wpart.h5')
+    if len(fns_wpart) > 0:
+        with h5.File(fns_wpart[0], 'r') as f:
+            if 'wpart/hi/charges' in f:
+                charges = f['wpart/hi/charges'][:]
+    # Create system object.
+    system = System(numbers, coords, charges=charges)
     system.detect_bonds()
     system.set_standard_masses()
+    # Load ab initio data.
     ai = SecondOrderTaylor('ai', coords=system.pos.copy(), energy=energy, grad=grad, hess=hess, pbc=pbc)
     return system, ai
+
+@contextmanager
+def tmpdir(name):
+    """Create a temporary directory where output files of a test can be written to.
+
+    The name argument can be used to obtained a recognizable test directory name. The
+    temporary directory and its contents are automatically removed.
+    """
+    dn = tempfile.mkdtemp(name)
+    try:
+        yield dn
+    finally:
+        shutil.rmtree(dn)
--- quickff/tests/test_examples.py
+++ quickff/tests/test_examples.py
@@ -1,16 +1,17 @@
+import numpy as np
+import os
+
 from molmod.units import *
 from molmod.constants import lightspeed
 from molmod.periodic import periodic as pt
 
-from quickff.tools import guess_ffatypes
-from quickff.program import DeriveNonDiagFF
-from quickff.context import context
-
 from yaff import System
 
-import numpy as np, os
+from quickff.tools import guess_ffatypes
+from quickff.program import DeriveNonDiagFF, DeriveDiagFF
+from quickff.context import context
 
-from common import log, read_system
+from common import log, read_system, tmpdir
 
 def test_h2():
     #frequency of H2 stretch mode in gaussian.fchk calculation is 4416.656/cm
@@ -42,3 +43,78 @@ def test_h2():
     assert abs(rv_hc/r0-1.0) < 1e-3
     assert abs(K_hc/K_pt-1.0) < 1e-6
     assert abs(rv_hc/rv_pt-1.0) < 1e-6
+
+
+def test_output_charmm22():
+    with log.section('NOSETST', 2):
+        system, ai = read_system('ethanol/gaussian.fchk')
+        guess_ffatypes(system, 'low')
+        with tmpdir('test_output_charmm22') as dn:
+            fn_yaff = os.path.join(dn, 'pars_cov.txt')
+            fn_charmm22_prm = os.path.join(dn, 'test.prm')
+            fn_charmm22_psf = os.path.join(dn, 'test.psf')
+            fn_sys = os.path.join(dn, 'system.chk')
+            program = DeriveDiagFF(system, ai, fn_yaff=fn_yaff,
+                                   fn_charmm22_prm=fn_charmm22_prm,
+                                   fn_charmm22_psf=fn_charmm22_psf,
+                                   fn_sys=fn_sys)
+            program.run()
+            assert os.path.isfile(fn_yaff)
+            assert os.path.isfile(fn_charmm22_prm)
+            assert os.path.isfile(fn_charmm22_psf)
+            assert os.path.isfile(fn_sys)
+
+            # Count the number of BOND, ANGLES and DIHEDRAL lines in the PRM file.
+            counts = {}
+            with open(fn_charmm22_prm, 'r') as f:
+                for line in f:
+                    line = line[:line.find('!')].strip()
+                    if len(line) == 0:
+                        continue
+                    if line in ['BONDS','ANGLES', 'DIHEDRALS', 'IMPROPER']:
+                        key = line
+                        counts[key] = 0
+                    else:
+                        counts[key] += 1
+            assert counts['BONDS'] == 4
+            assert counts['ANGLES'] == 5
+            assert counts['DIHEDRALS'] == 2
+            assert counts['IMPROPER'] == 0
+
+            # Count the number atoms, bonds, angles and dihedrals in the PSF file and
+            # check for consistency.
+            with open(fn_charmm22_psf, 'r') as f:
+                natom = 0
+                assert f.next() == 'PSF\n'
+                for line in f:
+                    if '!NATOM' in line:
+                        natom = int(line.split()[0])
+                        break
+                assert natom == system.natom
+                for iatom in xrange(natom+1):
+                    f.next()
+                line = f.next()
+                assert '!NBOND: bonds' in line
+                nbond = int(line.split()[0])
+                nline = int(np.ceil(nbond/4.0))
+                numbers = (''.join([f.next() for iline in xrange(nline)])).split()
+                assert len(numbers) == nbond*2
+                f.next()
+                line = f.next()
+                assert '!NTHETA: angles' in line
+                ntheta = int(line.split()[0])
+                nline = int(np.ceil(ntheta/3.0))
+                numbers = (''.join([f.next() for iline in xrange(nline)])).split()
+                assert len(numbers) == ntheta*3
+                f.next()
+                line = f.next()
+                assert '!NPHI: dihedrals' in line
+                nphi = int(line.split()[0])
+                nline = int(np.ceil(nphi/2.0))
+                numbers = (''.join([f.next() for iline in xrange(nline)])).split()
+                assert len(numbers) == nphi*4
+                f.next()
+                line = f.next()
+                assert '!NIMPHI: impropers' in line
+                nimphi = int(line.split()[0])
+                assert nimphi == 0
