89a4e732467c7867bd8fa2d4395244c67a42ec49
Louis Vanduyfhuys <Louis.Vanduyfhuys@UGent.be>
2017-01-18 18:26:22 +0100

    Temporary re-add updatesite.sh

---
+++ updatesite.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+git checkout githubmaster && (
+  ./setup.py install --user
+  ./cleanfiles.sh
+  (
+    cd doc
+    make html
+    cd _build/html
+    touch .nojekyll
+  )
+  git checkout gh-pages && (
+    git rm $(git ls-tree $(git log --color=never | head -n1 | cut -f2 -d' ') -r --name-only)
+    cp -rv doc/_build/html/* doc/_build/html/.* .
+    for f in $(find doc/_build/html | cut -c17-); do
+      echo Adding $f
+      git add $f
+    done
+    git commit -a -m 'Automatic documentation update'
+    git push github gh-pages:gh-pages
+  )
+) && git checkout githubmaster
